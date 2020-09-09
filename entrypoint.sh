a937efcd20566ba413f788917cd2549f79c73b66
Firdaus Maarof <firdausmaarof93@gmail.com>
2020-09-09 11:34:59 +0800

    add migration to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "Creating database if it's not present..."
+bin/rails db:create
+
+echo "Migrating database..."
+bin/rails db:migrate
+
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
